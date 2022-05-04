813bd28b0317db73cabb798d9f2ce6c5066ddaa8
Chandan Mourya <chandanmourya4786@gmail.com>
2022-05-04 14:07:47 +0530

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-//import { navbar } from "./component/navbar.js";
+import { navbar } from "./component/navbar.js";
 
-import {navbar} from "../Sephora-Clone/component/navbar.js"
+//import {navbar} from "../Sephora-Clone/component/navbar.js"
+//import {navbar} from "../Sephora-Clone/component/navbar.js"
 
 let n=document.getElementById("navbar");
 n.innerHTML = navbar();
