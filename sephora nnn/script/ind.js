36c9ea739ee18e47f9d996240a8417a1382fb66b
Aditya Sharma <101574252+T0biUchiha@users.noreply.github.com>
2022-05-07 12:07:45 +0530

    Add files via upload

--- sephora nnn/script/ind.js
+++ sephora nnn/script/ind.js
@@ -1,7 +1,10 @@
 let prd_deatils = JSON.parse(localStorage.getItem("prd_details"));
 
     let container = document.getElementById("main");
-    // let div = document.createElement("div");
+     let div = document.createElement("div");
+     div.setAttribute("id","ind_txt")
+     let div1 = document.createElement("div");
+     div1.setAttribute("id","ind_img")
     let title = document.createElement("h3");
     title.innerText= prd_deatils.title;
     title.setAttribute("id","prd_title");
@@ -18,15 +21,25 @@ let prd_deatils = JSON.parse(localStorage.getItem("prd_details"));
      color.innerText = "Color: "+ prd_deatils.swatches[0].colorName 
      let color_box = document.createElement("div");
      color_box.setAttribute("id","color_box");
-     color_box.style.height = "20px";
-     color_box.style.width = "20px";
      color_box.style.backgroundColor = prd_deatils.swatches[0].colorCode;
      price.innerText= prd_deatils.price;
      let add_cart = document.createElement("button");
      add_cart.setAttribute("id","cartbtn");
-     add_cart.innerText = "ADD TO BASKET"
+     add_cart.innerText = "ADD TO BAG"
+     add_cart.addEventListener("click",function(){
+        localStorage.setItem("cart",JSON.stringify(prd_deatils));
+        alert("Item added to Cart")
+     })
      let add_fav = document.createElement("button");
      add_fav.setAttribute("id","favbtn");
      add_fav.innerText = "ADD TO FAVOURITE"
-    //  div.append(img,title,price,color,color_box,add_cart,add_fav);
-     container.append(img,brand,title,price,color,color_box,add_cart,add_fav);
+     add_fav.addEventListener("click",function(){
+        localStorage.setItem("favourite",JSON.stringify(prd_deatils));
+        alert("Added to Favourites")
+     })
+     let ind_txt = document.createElement("p")
+     ind_txt.innerText ="ONLY EXCLUSIVE";
+     ind_txt.setAttribute("id","ind_text");
+      div.append(ind_txt,brand,title,price,color,color_box,add_cart,add_fav);
+      div1.append(img)
+     container.append(div1,div);
