1c5f201a42589a163fbe4289683ae5d89b68c072
Aditya Sharma <101574252+T0biUchiha@users.noreply.github.com>
2022-05-07 18:37:14 +0530

    Add files via upload

--- sephora nnn/script/ind.js
+++ sephora nnn/script/ind.js
@@ -1,4 +1,9 @@
 let prd_deatils = JSON.parse(localStorage.getItem("prd_details"));
+let array =JSON.parse(localStorage.getItem("cart")) || [];
+// 
+
+//console.log(array);
+console.log(prd_deatils);
 
     let container = document.getElementById("main");
      let div = document.createElement("div");
@@ -27,19 +32,42 @@ let prd_deatils = JSON.parse(localStorage.getItem("prd_details"));
      add_cart.setAttribute("id","cartbtn");
      add_cart.innerText = "ADD TO BAG"
      add_cart.addEventListener("click",function(){
-        localStorage.setItem("cart",JSON.stringify(prd_deatils));
-        alert("Item added to Cart")
+      
+            moveFunction(prd_deatils)
+
+         // array.push(prd_deatils);
+         // localStorage.setItem("cart",JSON.stringify(array));
+         // alert("Item added to Cart")
+        //localStorage.setItem("cart",JSON.stringify(prd_deatils));
+        
      })
-     let add_fav = document.createElement("button");
+     let add_fav = document.createElement("img");
      add_fav.setAttribute("id","favbtn");
-     add_fav.innerText = "ADD TO FAVOURITE"
+     add_fav.src = "https://cdn-icons-png.flaticon.com/512/3237/3237404.png"
      add_fav.addEventListener("click",function(){
         localStorage.setItem("favourite",JSON.stringify(prd_deatils));
         alert("Added to Favourites")
      })
+     let add_share = document.createElement("img");
+     add_share.setAttribute("id","sharebtn");
+     add_share.src = "https://cdn-icons-png.flaticon.com/512/6460/6460954.png"
      let ind_txt = document.createElement("p")
      ind_txt.innerText ="ONLY EXCLUSIVE";
      ind_txt.setAttribute("id","ind_text");
-      div.append(ind_txt,brand,title,price,color,color_box,add_cart,add_fav);
+      div.append(ind_txt,brand,title,price,color,color_box,add_cart,add_fav,add_share);
       div1.append(img)
      container.append(div1,div);
+
+
+     function moveFunction(prd_deatils){
+        let obj={
+           title:prd_deatils.title,
+           brandname:prd_deatils.brandName,
+            image:prd_deatils.image[0].dataAltImage,
+            price:prd_deatils.price,
+            color:prd_deatils.swatches[0].colorName,
+        }
+        array.push(obj)
+      localStorage.setItem("cart",JSON.stringify(array));
+      alert("Item added to Cart")
+     }
